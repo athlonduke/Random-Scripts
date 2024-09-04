8ebdb70d0c6f25e28f04e941c90ef14004f93b6c
athlonduke <78453885+athlonduke@users.noreply.github.com>
2024-09-04 17:26:27 -0400

    Create install-powershell7.ps1

---
+++ install-powershell7.ps1
@@ -0,0 +1,2 @@
+# stolen from here: https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.4
+winget install --id Microsoft.PowerShell --source winget
